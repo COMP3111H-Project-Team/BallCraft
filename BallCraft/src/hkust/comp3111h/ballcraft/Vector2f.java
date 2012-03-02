ad6da1eb13f4ed59d5124c6caa7d8def866264f7
liligeng111 <liligeng111@gmail.com>
2012-03-02 22:55:09 +0800

    bug fix

--- BallCraft/src/hkust/comp3111h/ballcraft/Vector2f.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/Vector2f.java
@@ -1,9 +1,5 @@
 package hkust.comp3111h.ballcraft;
 
-<<<<<<< HEAD
-public class Vector2f 
-{
-=======
 import java.io.Serializable;
 
 public class Vector2f implements Serializable
@@ -13,7 +9,6 @@ public class Vector2f implements Serializable
 	 */
 	private static final long serialVersionUID = 7481119251470929286L;
 	
->>>>>>> origin/master
 	public float x;
 	public float y;
 	
