728b71a3fb8f99ceead24c36c8d42c78e2d0a764
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-03-04 23:44:41 +0800

    balled drawn

--- BallCraft/src/hkust/comp3111h/ballcraft/MyRenderer.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/MyRenderer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.StreamCorruptedException;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -12,6 +13,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
+import android.util.Log;
 
 public class MyRenderer implements GLSurfaceView.Renderer {
 	
@@ -21,12 +23,14 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 	private Cube cube = null;
 	private Sphere sphere = null;
 	
+	private long time = 0;
+	
 	public MyRenderer(Client client) {
 		this.client = client;
 		
 		gamePlane = new Plane();
 		cube = new Cube();
-		sphere = new Sphere(15, 25);
+		sphere = new Sphere(40, 40, 30);
 	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -36,6 +40,27 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 1000.0f);
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
 		gl.glLoadIdentity();
+		
+		/*
+		float lightAmbient [] = { 0.1f, 0.1f, 0.1f, 1.0f };
+		float lightDiffuse [] = { 0.9f, 0.9f, 0.9f, 1.0f };
+		float lightPos [] = {100f, 100f, -50f, 1f};
+		
+		float matAmbient [] = { 0.6f, 0.6f, 0.6f, 1.0f };
+		float matDiffuse [] = { 1f, 1f, 1f, 1f };
+
+		gl.glEnable(GL10.GL_LIGHTING);
+		gl.glEnable(GL10.GL_LIGHT0);
+		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, matAmbient, 0);
+		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, matDiffuse, 0);
+
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, lightAmbient, 0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightDiffuse, 0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, lightPos, 0);
+
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		*/
 	}
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -45,10 +70,62 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 		gl.glEnable(GL10.GL_DEPTH_TEST);
 		gl.glDepthFunc(GL10.GL_LEQUAL);
 		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+		
+		FloatBuffer global_ambient = FloatBuffer.allocate(4);
+        global_ambient.put(0.5f);
+        global_ambient.put(0.5f);
+        global_ambient.put(0.5f);
+        global_ambient.put(1.0f);
+        
+        FloatBuffer position = FloatBuffer.allocate(4);
+        position.put(10f);
+        position.put(10f);
+        position.put(10f);
+        position.put(1f);
+        
+        /*
+        gl.glEnable(GL10.GL_LIGHTING);
+        gl.glEnable(GL10.GL_LIGHT0);
+        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, position);
+        
+        float[] mycolor = { 0.8f, 0.7f, 0.6f, 1.0f };
+        gl.glMaterialfv(GL10.GL_FRONT, GL10.GL_AMBIENT_AND_DIFFUSE, 
+        		mycolor,
+        		0);
+		*/
+		
+		/*
+		gl.glEnable(GL10.GL_LIGHT0);
+		
+		float [] sunlightDir = { 0.0f, 2.0f, -1.0f, 1.0f };
+		ByteBuffer bb = ByteBuffer.allocateDirect(sunlightDir.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+		FloatBuffer fb = bb.asFloatBuffer();
+		fb.put(sunlightDir);
+		fb.position(0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, fb);
+		
+		float [] sunlightInt = { 0.7f, 0.7f, 0.7f, 1.0f };
+		bb = ByteBuffer.allocateDirect(sunlightDir.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+		fb = bb.asFloatBuffer();
+		fb.put(sunlightDir);
+		fb.position(0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, fb);
+		
+		gl.glEnable(GL10.GL_COLOR_MATERIAL);
+		gl.glMaterialfv(GL10.GL_FRONT, GL10.GL_AMBIENT_AND_DIFFUSE, fb);
+		*/
 	}
 
 	@SuppressWarnings("unchecked")
 	public void onDrawFrame(GL10 gl) {
+		
+		/*
+		long elapsed = System.currentTimeMillis() - time;
+		Log.d("fps", "" + 1000 / elapsed);
+		time = System.currentTimeMillis();
+		*/
 
 		ArrayList<UnitData> data = null;
 		byte[] bytes = Server.process(0, client.getInput());
@@ -88,9 +165,9 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 			if (datum.identity == Unit.type.BALL) {
 				gl.glPushMatrix();
 					gl.glTranslatef(datum.position.x, - datum.position.y, 5);
-					gl.glColor4f(1f, 0f, 0f, 1f);
-					gl.glScalef(datum.size / 15, datum.size / 15, datum.size / 15);
-					cube.draw(gl);
+					gl.glColor4f(1f, 1f, 1f, 1f);
+					gl.glScalef(datum.size, datum.size, datum.size);
+					sphere.draw(gl);
 				gl.glPopMatrix();
 			}
 		}
--- BallCraft/src/hkust/comp3111h/ballcraft/Sphere.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/Sphere.java
@@ -7,53 +7,114 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 public class Sphere {
-
-    static private FloatBuffer sphereVertex;
-    static private FloatBuffer sphereNormal;
-    static float sphere_parms[]=new float[3];
-
-    double mRaduis;
-    double mStep;
-    float mVertices[];
-    private static double DEG = Math.PI/180;
-    int mPoints;
-
-    public Sphere( float radius, double step) {
-        this.mRaduis = radius;
-        this.mStep = step;
-        ByteBuffer bb = ByteBuffer.allocateDirect(40000);
-        bb.order(ByteOrder.nativeOrder());
-        sphereVertex = bb.asFloatBuffer();
-        mPoints = build();
+    FloatBuffer strip, fan_top, fan_bottom;
+    float radius;
+    int stacks,  slices;
+ 
+    public Sphere(int stacks, int slices, float radius) {
+        this.stacks = stacks;
+        this.slices = slices;
+        this.radius = radius;
+        unitSphere(stacks, slices);
     }
-
+ 
     public void draw(GL10 gl) {
-        gl.glFrontFace(GL10.GL_CW);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, fan_top);
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, sphereVertex);
-
-        gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-        gl.glDrawArrays(GL10.GL_POINTS, 0, mPoints);
-        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+ 
+        gl.glNormalPointer(GL10.GL_FLOAT, 0, fan_top);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, slices + 2);
+ 
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, strip);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+ 
+        gl.glNormalPointer(GL10.GL_FLOAT, 0, strip);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, (slices + 1) * 2 * stacks);
+ 
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, fan_bottom);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+ 
+        gl.glNormalPointer(GL10.GL_FLOAT, 0, fan_bottom);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, slices + 2);
     }
-
-    private int build() {
-
-        double dTheta = mStep * DEG;
-        double dPhi = dTheta;
-        int points = 0;
-
-        for(double phi = -(Math.PI); phi <= Math.PI/2; phi+=dPhi) {
-            //for each stage calculating the slices
-            for(double theta = 0.0; theta <= (Math.PI * 2); theta+=dTheta) {
-                sphereVertex.put((float) (mRaduis * Math.sin(phi) * Math.cos(theta)) );
-                sphereVertex.put((float) (mRaduis * Math.sin(phi) * Math.sin(theta)) );
-                sphereVertex.put((float) (mRaduis * Math.cos(phi)) );
-                points++;
-
+ 
+    protected FloatBuffer makeEndCap(int stacks, int slices, boolean top) {
+        // Calculate the Triangle Fan for the endcaps
+        int triangleFanVertexCount = slices + 2;
+        float dtheta = (float)(2.0 * Math.PI / slices);
+        float drho =  (float)(Math.PI / stacks);
+        float[] fanVertices = new float[triangleFanVertexCount * 3];
+        float theta = 0;
+        float sin_drho = (float)Math.sin(drho);
+        //float cos_drho = (float)Math.cos(Math.PI / stacks);
+        int index = 0;
+        fanVertices[index++] = 0.0f;
+        fanVertices[index++] = 0.0f;
+        fanVertices[index++] = (top ? 1 : -1);
+        for (int j = 0; j <= slices; j++)
+        {
+            theta = (j == slices) ? 0.0f : j * (top ? 1 : -1) * dtheta;
+            float x = (float)-Math.sin(theta) * sin_drho;
+            float y = (float)Math.cos(theta) * sin_drho;
+            float z = (top ? 1 : -1) * (float)Math.cos(drho);
+            fanVertices[index++] = x;
+            fanVertices[index++] = y;
+            fanVertices[index++] = z;
+ 
+        }
+ 
+        return makeFloatBuffer(fanVertices);
+    }
+ 
+    protected void unitSphere(int stacks, int slices) {
+        float drho =  (float)(Math.PI / stacks);
+        float dtheta = (float)(2.0 * Math.PI / slices);
+ 
+        fan_top = makeEndCap(stacks, slices, true);
+        fan_bottom = makeEndCap(stacks, slices, false);
+ 
+        // Calculate the triangle strip for the sphere body
+        int triangleStripVertexCount = (slices + 1) * 2 * stacks;
+        float[] stripVertices = new float[triangleStripVertexCount * 3];
+ 
+        int index = 0;
+        for (int i = 0; i < stacks; i++) {
+            float rho = i * drho;
+ 
+            for (int j = 0; j <= slices; j++)
+            {
+                float theta = (j == slices) ? 0.0f : j * dtheta;
+                float x = (float)(-Math.sin(theta) * Math.sin(rho));
+                float y = (float)(Math.cos(theta) * Math.sin(rho));
+                float z = (float)Math.cos(rho);
+                // TODO: Implement texture mapping if texture used
+                //                TXTR_COORD(s, t);
+                stripVertices[index++] = x;
+                stripVertices[index++] = y;
+                stripVertices[index++] = z;
+ 
+                x = (float)(-Math.sin(theta) * Math.sin(rho + drho));
+                y = (float)(Math.cos(theta) * Math.sin(rho + drho));
+                z = (float)Math.cos(rho + drho);
+                // TODO: Implement texture mapping if texture used
+                //                TXTR_COORD(s, t);
+                stripVertices[index++] = x;
+                stripVertices[index++] = y;
+                stripVertices[index++] = z;
             }
         }
-        sphereVertex.position(0);
-        return points;
+        strip = makeFloatBuffer(stripVertices);
+    }
+ 
+    private FloatBuffer makeFloatBuffer(float[] arr){
+        ByteBuffer bb = ByteBuffer.allocateDirect(arr.length*4);
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer fb = bb.asFloatBuffer();
+        fb.put(arr);
+        fb.position(0);
+        return fb;
     }
 }
