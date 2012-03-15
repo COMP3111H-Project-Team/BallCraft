a9e977d5b13d1e55ded8d6ee1c27eeaf060f8b49
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-03-15 11:20:39 +0800

    start refactoring

---
+++ BallCraft/src/hkust/comp3111h/ballcraft/client/GameActivity.java
@@ -0,0 +1,201 @@
+package hkust.comp3111h.ballcraft.client;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLDisplay;
+
+import hkust.comp3111h.ballcraft.BallCraft;
+import hkust.comp3111h.ballcraft.graphics.GameRenderer;
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class GameActivity extends Activity implements SensorEventListener {
+	
+	private GLSurfaceView mGLView;
+	private GameRenderer renderer = null;
+	private SensorManager sensorManager;
+	private RelativeLayout rLayout;
+	
+	private static TextView debugView = null;
+	private static String debugMsg = null;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, 
+    			WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    	super.onCreate(savedInstanceState);
+    	
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		initLayout();
+		
+		initSensor();
+		
+		renderer = new GameRenderer();
+		mGLView.setRenderer(renderer);
+		
+		setContentView(rLayout);
+		
+    }
+    
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mGLView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mGLView.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+    
+    /**
+     * Initialize the main layout of the game view,
+     * including buttons, MUD, etc.
+     */
+    private void initLayout() {
+    	
+		mGLView = new GLSurfaceView(getApplication());
+
+		mGLView.setEGLConfigChooser(new GLSurfaceView.EGLConfigChooser() {
+			public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
+				int[] attributes = new int[] { EGL10.EGL_DEPTH_SIZE, 16, EGL10.EGL_NONE };
+				EGLConfig[] configs = new EGLConfig[1];
+				int[] result = new int[1];
+				egl.eglChooseConfig(display, attributes, configs, 1, result);
+				return configs[0];
+			}
+		});
+		
+		rLayout = new RelativeLayout(this);
+		rLayout.setLayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.FILL_PARENT,
+				ViewGroup.LayoutParams.FILL_PARENT));
+		
+		Button castBtn = new Button(this);
+		castBtn.setLayoutParams(new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT));
+		castBtn.setPadding(60, 50, 60, 50);
+		castBtn.getBackground().setAlpha(80);
+		castBtn.setText("Function 1");
+		castBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*
+				if (input != null) {
+					input.addSkill(new Skill(BallCraft.Skill.TEST_SKILL_1));
+				}
+				*/
+			}
+		});
+		
+		Button accBtn = new Button(this);
+		RelativeLayout.LayoutParams accParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		accParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		accBtn.setLayoutParams(accParams);
+		accBtn.setPadding(60, 50, 60, 50);
+		accBtn.getBackground().setAlpha(80);
+		accBtn.setText("Function 2");
+		accBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*
+				input.addSkill(new Skill(BallCraft.Skill.TEST_SKILL_2));
+				*/
+			}
+		});
+		
+		debugView = new TextView(this);
+		RelativeLayout.LayoutParams debugParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		debugParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		debugView.setLayoutParams(debugParams);
+		debugView.setPadding(10, 10, 10, 10);
+		debugView.setTextColor(Color.GREEN);
+		debugView.setText("Debug Message Display");
+		
+		rLayout.addView(mGLView);
+		rLayout.addView(castBtn);
+		rLayout.addView(accBtn);
+		rLayout.addView(debugView);
+    }
+    
+    private void initSensor() {
+		sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+		sensorManager.registerListener(this, 
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		
+	}
+	
+	/**
+	 * Used for displaying a debug message at the bottom of the screen
+	 */
+	public static void displayDebugMsg() {
+		debugView.setText(debugMsg);
+	}
+	
+	private static Handler debugMsgHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			displayDebugMsg();
+		}
+	};
+	
+	public static void display(String msg) {
+		debugMsg = msg;
+		debugMsgHandler.sendEmptyMessage(0);
+	}
+	
+	public static void display(float msg) {
+		debugMsg = "" + msg;
+		debugMsgHandler.sendEmptyMessage(0);
+	}
+	
+	public static void display(double msg) {
+		debugMsg = "" + msg;
+		debugMsgHandler.sendEmptyMessage(0);
+	}
+	
+	public static void display(int msg) {
+		debugMsg = "" + msg;
+		debugMsgHandler.sendEmptyMessage(0);
+	}
+}
---
+++ BallCraft/src/hkust/comp3111h/ballcraft/graphics/GameRenderer.java
@@ -0,0 +1,186 @@
+package hkust.comp3111h.ballcraft.graphics;
+
+import hkust.comp3111h.ballcraft.BallCraft;
+import hkust.comp3111h.ballcraft.client.Client;
+import hkust.comp3111h.ballcraft.server.ServerAdapter;
+import hkust.comp3111h.ballcraft.server.Unit;
+import hkust.comp3111h.ballcraft.server.UnitData;
+import hkust.comp3111h.ballcraft.server.Vector2f;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+public class GameRenderer implements GLSurfaceView.Renderer {
+	
+	// private Client client = null;
+	
+	private Plane gamePlane = null;
+	private Sphere sphere = null;
+	
+	private Wall wall = null;
+	
+	private long time = 0;
+	
+	private boolean skillActivated = false;
+	private ParticleSystem system = null;
+	
+	public GameRenderer() {
+		// this.client = client;
+		
+		gamePlane = new Plane();
+		sphere = new Sphere(30, 30, 1);
+		wall = new Wall(new Vector2f(0, 0), new Vector2f(100, 100));
+	}
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 800f);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		
+		/*
+		gl.glEnable(GL10.GL_LIGHTING);
+		gl.glEnable(GL10.GL_LIGHT0);
+		
+		float lightAmbient [] = { 0.2f, 0.2f, 0.2f, 1.0f };
+		float lightDiffuse [] = { 0.9f, 0.9f, 0.9f, 1.0f };
+		float lightSpecular [] = { 0.9f, 0.9f, 0.9f, 1.0f };
+		float lightPosition [] = { 0f, 0f, 100f, 1f };
+		
+		float matSpecular [] = { 1f, 1f, 1f, 1f };
+		float matShininess [] = { 5.0f };
+		float matAmbient [] = { 1f, 1f, 1f, 1f };
+		float matDiffuse [] = { 1f, 1f, 1f, 1f };
+		
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		
+		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, matSpecular, 0);
+		// gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SHININESS, matShininess, 0);
+		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_AMBIENT, matAmbient, 0);
+		gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_DIFFUSE, matDiffuse, 0);
+		
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_SPECULAR, lightSpecular, 0);
+		// gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, lightPosition, 0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, lightAmbient, 0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightDiffuse, 0);
+		*/
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// gamePlane.loadTexture(gl, client); // tex
+		gl.glEnable(GL10.GL_TEXTURE_2D); //tex
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glClearDepthf(1.0f);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL10.GL_LEQUAL);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void onDrawFrame(GL10 gl) {
+		
+		/*
+		long elapsed = System.currentTimeMillis() - time;
+		Client.display("fps: " + 1000 / elapsed);
+		time = System.currentTimeMillis();
+		*/
+		
+		/*
+		ArrayList<UnitData> data = null;
+		byte[] bytes = ServerAdapter.process(0, client.getInput());
+		
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		ObjectInput in;
+		
+		try {
+			in = new ObjectInputStream(bis);
+			data = (ArrayList<UnitData>) in.readObject();
+			bis.close();
+			in.close();
+		} catch (StreamCorruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		UnitData self = data.get(0);
+		
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+		
+		GLU.gluLookAt(gl, 
+				self.position.x, -self.position.y - 200, 300, 
+				self.position.x, -self.position.y, 5, 
+				0, 0, 1);
+		*/
+		
+		/*
+		float xPos = self.position.x;
+		float yPos = - self.position.y;
+		float xVel = self.velocity.x;
+		float yVel = - self.velocity.y;
+		double vel = 200; // Math.sqrt(xVel * xVel + yVel * yVel) * 100;
+		double tanAngle = xVel / yVel;
+		double angle = Math.atan(tanAngle);
+		float xDiff = (float) (Math.sin(angle) * vel);
+		float yDiff = (float) (Math.cos(angle) * vel);
+	
+		if (xVel > 0 && yVel > 0) { // first quadrant
+			GLU.gluLookAt(gl, xPos - xDiff, yPos - yDiff, 300, xPos, yPos, 5f, 0, 0, 1f);
+		} else if (xVel < 0 && yVel > 0) { // second quadrant
+			GLU.gluLookAt(gl, xPos - xDiff, yPos - yDiff, 300, xPos, yPos, 5f, 0, 0, 1f);
+		} else if (xVel < 0 && yVel < 0) { // third quadrant
+			GLU.gluLookAt(gl, xPos + xDiff, yPos + yDiff, 300, xPos, yPos, 5f, 0, 0, 1f);
+		} else if (xVel > 0 && yVel < 0) { // fourth quadrant
+			GLU.gluLookAt(gl, xPos + xDiff, yPos + yDiff, 300, xPos, yPos, 5f, 0, 0, 1f);
+		} else {
+			gl.glTranslatef(-self.position.x, self.position.y, -300);
+		}
+		*/
+		
+		/*
+		gl.glPushMatrix();
+			gl.glColor4f(0.6f, 0.6f, 0.6f, 1f);
+			gamePlane.draw(gl);
+		gl.glPopMatrix();
+				
+		for (int i = 0; i < data.size(); i++) {
+			UnitData datum = data.get(i);
+			if (datum.identity == Unit.type.BALL) {
+				gl.glPushMatrix();
+					gl.glTranslatef(datum.position.x, - datum.position.y, 10);
+					gl.glColor4f(0f, 0f, 1f, 1f);
+					gl.glScalef(datum.size, datum.size, datum.size);
+					sphere.draw(gl);
+				gl.glPopMatrix();
+			}
+		}
+		
+		gl.glColor4f(1f, 1f, 0f, 1f);
+		wall.draw(gl);
+		
+		
+		if (ServerAdapter.skillActive()) {
+			skillActivated = true;
+			if (ServerAdapter.getSkill() == BallCraft.Skill.TEST_SKILL_1) {
+				system = new ParticleSystem1(data.get(0).position.x, -data.get(0).position.y, 0);
+			} else if (ServerAdapter.getSkill() == BallCraft.Skill.TEST_SKILL_2) {
+				system = new ParticleSystem2(data.get(0).position.x, -data.get(0).position.y, 0);
+			}
+		}
+		
+		if (skillActivated) {
+			system.move();
+			system.draw(gl);
+		}
+		*/
+	}
+}
