1f6a2ab292fb0d354ca541a017d1f16b0441fa36
liligeng111 <liligeng111@gmail.com>
2012-03-18 15:14:48 +0800

    refactor

--- BallCraft/src/hkust/comp3111h/ballcraft/server/Ball.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/server/Ball.java
@@ -2,12 +2,29 @@ package hkust.comp3111h.ballcraft.server;
 
 import javax.microedition.khronos.opengles.GL10;
 
+import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 
 public class Ball extends Unit {
 	
 	public Ball(float size, float mass, float friction, Vec2 position) {
-		super(size, mass, friction, position);
+		super();
+		
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DYNAMIC; // dynamic means it is subject to forces
+		bodyDef.position.set(position.x, position.y);
+		body = ServerGameState.world.createBody(bodyDef);
+		CircleShape shape = new CircleShape();
+		shape.m_radius = size;
+		
+		FixtureDef fixtureDef = new FixtureDef(); // fixture def that we load up with the following info:
+		fixtureDef.shape = shape; // ... its shape is the dynamic box (2x2 rectangle)
+		fixtureDef.density = mass; // ... its density is 1 (default is zero)
+		fixtureDef.friction = friction; // ... its surface has some friction coefficient
+		body.createFixture(fixtureDef); // bind the dense, friction-laden fixture to the body
 	}
 
 	@Override
--- BallCraft/src/hkust/comp3111h/ballcraft/server/ServerAdapter.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/server/ServerAdapter.java
@@ -3,15 +3,12 @@ package hkust.comp3111h.ballcraft.server;
 import hkust.comp3111h.ballcraft.client.GameInput;
 
 public class ServerAdapter {
-	
-	public ServerAdapter() {
+		
+	public static void sendToServer(GameInput input) {
+		Server.process(input.toSerializedString());
 	}
 	
-	public void sendToServer(GameInput input) {
-		// TODO: serialize input and send to server
-	}
-	
-	public void onMessage(Byte [] bytes) {
+	public static void onMessage(Byte [] bytes) {
 		// dummy function, deserialize and send to client
 	}
 	
