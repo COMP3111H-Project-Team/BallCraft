4883d373016783526e0867a673eb0bca8e8e6d0b
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-03-20 00:35:55 +0800

    menu improved

--- BallCraft/src/hkust/comp3111h/ballcraft/client/GameActivity.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/client/GameActivity.java
@@ -18,7 +18,6 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
