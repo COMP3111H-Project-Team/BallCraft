9d97a93102f6867ee7e0a000183751ab9500b200
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-04-18 12:49:54 +0800

    dark ball particle system implemented

---
+++ BallCraft/src/hkust/comp3111h/ballcraft/graphics/particles/Particle.java
@@ -0,0 +1,156 @@
+package hkust.comp3111h.ballcraft.graphics.particles;
+
+import hkust.comp3111h.ballcraft.graphics.Drawable;
+import hkust.comp3111h.ballcraft.graphics.Vec3;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+
+public abstract class Particle implements Drawable, Comparable<Object> {
+    
+    protected static FloatBuffer vertexBuffer;
+    protected static FloatBuffer textureBuffer;
+    
+    protected static final float [] vertices = {
+            -1, -1, 0,
+            -1, 1, 0,
+            1, -1, 0,
+            1, 1, 0,
+    };
+    
+    protected static final float[] texture = { 
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+    };
+    
+    protected static final float gravity = -0.1f;
+    
+    public float x = 0;
+    public float y = 0;
+    public float z = 0;
+    
+    public float xSpeed = 0;
+    public float ySpeed = 0;
+    public float zSpeed = 0;
+    
+    protected float size;
+    
+    protected boolean gravityInfluence;
+    
+    static {
+        vertexBuffer = makeVertexBuffer();
+        textureBuffer = makeTextureBuffer();
+    }
+
+    public Particle(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public Particle(float x, float y, float z, float xSpeed, float ySpeed, float zSpeed) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.zSpeed = zSpeed;
+    }
+    
+    /**
+     * Set whether gravity influences the particle, if so the particle will have a z-direction acceleration
+     * @param gi Whether gravity influences
+     */
+    public void setGravityInfluence(boolean gi) {
+        this.gravityInfluence = gi;
+    }
+    
+    public void setSpeed(float xSpeed, float ySpeed, float zSpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.zSpeed = zSpeed;
+    }
+    
+    public Vec3 getSpeed() {
+        return new Vec3(xSpeed, ySpeed, zSpeed);
+    }
+    
+    public void setSize(float size) {
+        this.size = size;
+    }
+    
+    public float getSize() {
+        return this.size;
+    }
+
+    public void move() {
+        if (this.gravityInfluence) {
+	        this.zSpeed += gravity;
+        }
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        this.z += this.zSpeed;
+    }
+    
+    public void draw(GL10 gl) {
+        gl.glPushMatrix();
+        
+	        gl.glTranslatef(x, y, z);
+	        gl.glScalef(size, size, 1);
+	        gl.glEnable(GL10.GL_TEXTURE_2D);
+	        
+	        this.bindTexture(gl);
+	        
+	        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+	        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+	        
+	        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+	        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+	        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+	        
+	        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+	        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+	        
+	        gl.glDisable(GL10.GL_TEXTURE_2D);
+        
+	    gl.glPopMatrix();
+    }
+    
+    protected abstract void bindTexture(GL10 gl);
+    
+    private static FloatBuffer makeVertexBuffer() {
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer buffer = bb.asFloatBuffer();
+        buffer.put(vertices);
+        buffer.position(0);
+        return buffer;
+    }
+    
+    private static FloatBuffer makeTextureBuffer() {
+        ByteBuffer bb = ByteBuffer.allocateDirect(texture.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer buffer = bb.asFloatBuffer();
+        buffer.put(texture);
+        buffer.position(0);
+        return buffer;
+    }
+
+
+    public int compareTo(Object obj) {
+        Particle p = (Particle) obj;
+        if (this.z > p.z) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    
+}
--- BallCraft/src/hkust/comp3111h/ballcraft/server/Ball.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/server/Ball.java
@@ -1,13 +1,14 @@
 package hkust.comp3111h.ballcraft.server;
 
-import hkust.comp3111h.ballcraft.R;
+import hkust.comp3111h.ballcraft.BallCraft;
 import hkust.comp3111h.ballcraft.BallCraft.Status;
 import hkust.comp3111h.ballcraft.client.ClientGameState;
-import hkust.comp3111h.ballcraft.graphics.GraphicUtils;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
+import hkust.comp3111h.ballcraft.graphics.balls.DarkBall;
+import hkust.comp3111h.ballcraft.graphics.balls.FireBall;
+import hkust.comp3111h.ballcraft.graphics.balls.IronBall;
+import hkust.comp3111h.ballcraft.graphics.balls.RockBall;
+import hkust.comp3111h.ballcraft.graphics.balls.WaterBall;
+import hkust.comp3111h.ballcraft.graphics.balls.WoodBall;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -17,31 +18,38 @@ import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.opengl.GLUtils;
-
 public class Ball extends Unit {
 
     public float z;
     private float zv;
-
-    private FloatBuffer strip;
-    private FloatBuffer fan_top, fan_bottom;
-    // private FloatBuffer texture_top, texture_bottom;
     
-    private float [] texCoords = {
-            0, 1,
-            1, 1,
-            0, 0,
-            1, 0
-    };
-    
-    private static int [] textureIDs = new int [1];
-    
-    private int stacks = 20, slices = 20;
-    
+    public static Ball getTypedBall(Ball ball, int ballType) {
+        switch (ballType) {
+        
+        case BallCraft.Ball.WOOD_BALL:
+            return new WoodBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        case BallCraft.Ball.ROCK_BALL:
+            return new RockBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        case BallCraft.Ball.WATER_BALL:
+            return new WaterBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        case BallCraft.Ball.IRON_BALL:
+            return new IronBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        case BallCraft.Ball.FIRE_BALL:
+            return new FireBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        case BallCraft.Ball.DARK_BALL:
+            return new DarkBall(ball.getRadius(), ball.getPosition(), ball.z);
+            
+        default:
+            return ball;
+            
+        }
+    }
+   
     public Ball(float size, float mass, float friction, Vec2 position) {
         super();
         size /= rate;
@@ -79,130 +87,11 @@ public class Ball extends Unit {
         shape.m_radius = radius;
         body.createFixture(shape, 0); // bind the dense, friction-laden fixture
                                       // to the body
-
-        unitSphere(stacks, slices);
     }
 
     public void draw(GL10 gl) {
-        gl.glPushMatrix();
-
-        GraphicUtils.setMaterialColor(gl, new float[] { 0.0f, 0.4f, 0.8f, 1f });
-
-        gl.glTranslatef(this.getPosition().x, this.getPosition().y, getRadius() - z);
-        gl.glScalef(this.getRadius(), this.getRadius(), this.getRadius());
-
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
-        // gl.glEnabeClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-        // gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texture_top);
-        
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, fan_top);
-        gl.glNormalPointer(GL10.GL_FLOAT, 0, fan_top);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, slices + 2);
-
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, strip);
-        gl.glNormalPointer(GL10.GL_FLOAT, 0, strip);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, (slices + 1) * 2 * stacks);
-
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, fan_bottom);
-        gl.glNormalPointer(GL10.GL_FLOAT, 0, fan_bottom);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, slices + 2);
-        
-        gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
-        gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
-        // gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-
-        GraphicUtils.restoreMaterialColor(gl);
-
-        gl.glPopMatrix();
-    }
-
-    private FloatBuffer makeEndCap(int stacks, int slices, boolean top) {
-        // Calculate the Triangle Fan for the endcaps
-        int triangleFanVertexCount = slices + 2;
-        float dtheta = (float) (2.0 * Math.PI / slices);
-        float drho = (float) (Math.PI / stacks);
-        float [] fanVertices = new float[triangleFanVertexCount * 3];
-        float theta = 0;
-        float sin_drho = (float) Math.sin(drho);
-        // float cos_drho = (float)Math.cos(Math.PI / stacks);
-        
-        int index = 0;
-        fanVertices[index++] = 0.0f;
-        fanVertices[index++] = 0.0f;
-        fanVertices[index++] = (top ? 1 : -1);
-        for (int j = 0; j <= slices; j++) {
-            theta = (j == slices) ? 0.0f : j * (top ? 1 : -1) * dtheta;
-            float x = (float) -Math.sin(theta) * sin_drho;
-            float y = (float) Math.cos(theta) * sin_drho;
-            float z = (top ? 1 : -1) * (float) Math.cos(drho);
-            fanVertices[index++] = x;
-            fanVertices[index++] = y;
-            fanVertices[index++] = z;
-        }
-
-        return makeFloatBuffer(fanVertices);
     }
     
-    private FloatBuffer makeTextureBuffer() {
-        ByteBuffer tbb = ByteBuffer.allocateDirect(texCoords.length * 4);
-        tbb.order(ByteOrder.nativeOrder());
-        FloatBuffer texBuffer = tbb.asFloatBuffer();
-        texBuffer.put(texCoords);
-        texBuffer.position(0);
-        return texBuffer;
-    }
-
-    private void unitSphere(int stacks, int slices) {
-        float drho = (float) (Math.PI / stacks);
-        float dtheta = (float) (2.0 * Math.PI / slices);
-
-        fan_top = makeEndCap(stacks, slices, true);
-        fan_bottom = makeEndCap(stacks, slices, false);
-        
-        // texture_top = this.makeTextureBuffer();
-
-        // Calculate the triangle strip for the sphere body
-        int triangleStripVertexCount = (slices + 1) * 2 * stacks;
-        float[] stripVertices = new float[triangleStripVertexCount * 3];
-
-        int index = 0;
-        for (int i = 0; i < stacks; i++) {
-            float rho = i * drho;
-
-            for (int j = 0; j <= slices; j++) {
-                float theta = (j == slices) ? 0.0f : j * dtheta;
-                float x = (float) (-Math.sin(theta) * Math.sin(rho));
-                float y = (float) (Math.cos(theta) * Math.sin(rho));
-                float z = (float) Math.cos(rho);
-                // TODO: Implement texture mapping if texture used
-                // TXTR_COORD(s, t);
-                stripVertices[index++] = x;
-                stripVertices[index++] = y;
-                stripVertices[index++] = z;
-
-                x = (float) (-Math.sin(theta) * Math.sin(rho + drho));
-                y = (float) (Math.cos(theta) * Math.sin(rho + drho));
-                z = (float) Math.cos(rho + drho);
-                // TODO: Implement texture mapping if texture used
-                // TXTR_COORD(s, t);
-                stripVertices[index++] = x;
-                stripVertices[index++] = y;
-                stripVertices[index++] = z;
-            }
-        }
-        strip = makeFloatBuffer(stripVertices);
-    }
-
-    private FloatBuffer makeFloatBuffer(float[] arr) {
-        ByteBuffer bb = ByteBuffer.allocateDirect(arr.length * 4);
-        bb.order(ByteOrder.nativeOrder());
-        FloatBuffer fb = bb.asFloatBuffer();
-        fb.put(arr);
-        fb.position(0);
-        return fb;
-    }
-
     public String toSerializedString() {
         if (Math.abs(body.getPosition().x) > 200 / rate
                 || Math.abs(body.getPosition().y) > 200 / rate) {
@@ -241,17 +130,4 @@ public class Ball extends Unit {
         body.getFixtureList().m_shape.m_radius = radius;
         body.setTransform(new Vec2(x, y), 0);
     }
-    
-    public static void loadTexture(GL10 gl, Context context) {
-        Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.fire_wall);
-        
-        gl.glGenTextures(1, textureIDs, 0);
-        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureIDs[0]);
-        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
-        
-		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bmp, 0); 
-		bmp.recycle();
-    }
-
-}
+   }
