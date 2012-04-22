f79791fe30733abea8c241005d2635408e70b489
Dai Zuozhuo <daizuozhuo@gmail.com>
2012-04-22 18:49:49 +0800

    fix vibrate bug

--- BallCraft/src/hkust/comp3111h/ballcraft/server/bt/BluetoothService.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/server/bt/BluetoothService.java
@@ -2,7 +2,9 @@ package hkust.comp3111h.ballcraft.server.bt;
 
 import hkust.comp3111h.ballcraft.BallCraft;
 import hkust.comp3111h.ballcraft.client.Client;
+import hkust.comp3111h.ballcraft.client.ClientGameState;
 import hkust.comp3111h.ballcraft.server.Server;
+import hkust.comp3111h.ballcraft.server.ServerGameState;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -383,6 +385,11 @@ public class BluetoothService {
                         .sendToTarget();
             } catch (IOException e) {
                 Log.e(TAG, "Exception during write", e);
+                Server.stop();
+                Client.stop();
+                ClientGameState.clear();
+                if(BallCraft.isServer) ServerGameState.clear();
+                destroy();
             }
         }
 
