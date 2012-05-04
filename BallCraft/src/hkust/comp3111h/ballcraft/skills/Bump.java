3ee99888d3527a1a93002eca4caee28e4410ceb6
liligeng111 <liligeng111@gmail.com>
2012-05-04 19:36:53 +0800

    merge

--- BallCraft/src/hkust/comp3111h/ballcraft/skills/Bump.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/skills/Bump.java
@@ -3,6 +3,7 @@ package hkust.comp3111h.ballcraft.skills;
 import hkust.comp3111h.ballcraft.BallCraft;
 import hkust.comp3111h.ballcraft.BallCraft.Status;
 import hkust.comp3111h.ballcraft.SkillDef;
+import hkust.comp3111h.ballcraft.server.Server;
 
 public class Bump extends Skill{
 	
@@ -28,10 +29,12 @@ public class Bump extends Skill{
 	@Override
 	public void beforeStep()
 	{
-		if (!done && System.currentTimeMillis() - time > 1000)
+		if (!done && System.currentTimeMillis() - time > 500)
 		{
 			if (getUnit(1 - player).getStatus() == Status.INVINCIBLE) return;
 			getUnit(1 - player).setStatus(BallCraft.Status.DIZZY);
+			Server.extraMessage("RockBumpEffect:" + id + "&" + (1 - player));
+			done = true;
 		}
 	}
 
--- BallCraft/src/hkust/comp3111h/ballcraft/skills/Flashbang.java
+++ BallCraft/src/hkust/comp3111h/ballcraft/skills/Flashbang.java
@@ -1,28 +1,38 @@
 package hkust.comp3111h.ballcraft.skills;
 
+import hkust.comp3111h.ballcraft.BallCraft;
 import hkust.comp3111h.ballcraft.SkillDef;
+import hkust.comp3111h.ballcraft.BallCraft.Status;
+import hkust.comp3111h.ballcraft.server.Server;
 
 public class Flashbang extends Skill
 {
+	
+	boolean done;
 		
 	public Flashbang(int player, int id) 
 	{
         this.player = player;
         this.id = id;
         this.duration = SkillDef.FlashBang.effectTime;
-	}
-	
-	@Override
-	public String getInitMsg()
-	{
-		return "NILL";
+        done = false;
 	}
 	
 	@Override
 	public void init() {}
 
 	@Override
-	public void beforeStep() {}
+	public void beforeStep()
+	{
+		if (!done && System.currentTimeMillis() - time > 500)
+		{
+			if (getUnit(1 - player).getStatus() == Status.INVINCIBLE) return;
+			getUnit(1 - player).setStatus(BallCraft.Status.DIZZY);
+			Server.extraMessage("FlashBangEffect:" + id + "&" + (1 - player));
+			done = true;
+		}
+		
+	}
 
 	@Override
 	public void afterStep() {}
